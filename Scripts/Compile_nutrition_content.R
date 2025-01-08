20c3e9c7bcc6365ae347faee08e09edf7ce029a1
juliagmason <juliagmason@gmail.com>
2025-01-08 13:39:41 -0500

    All missing species in DBEM spp list accounted for by harmonizing species names or taking average from genus or family

--- Scripts/Compile_nutrition_content.R
+++ Scripts/Compile_nutrition_content.R
@@ -14,9 +14,7 @@ firstup <- function(x) {
   x
 }
 
-spp_list <- read_csv("Data/mcp_full.csv") %>%
-  select (taxon_name) %>%
-  unique() %>%
+spp_list <- read_csv("Data/dbem_spp_list.csv") %>%
   # create matching column for missing species, found below
   mutate(afcd_name = case_when (
     grepl("Fenneropenaeus", taxon_name) ~ str_replace (taxon_name, "Fenneropenaeus", "Penaeus"),
@@ -26,6 +24,14 @@ spp_list <- read_csv("Data/mcp_full.csv") %>%
     taxon_name == "Moolgarda seheli" ~ "Crenimugil seheli",
     taxon_name == "Theragra chalcogramma" ~"Gadus chalcogrammus",
     taxon_name == "Trigloporus lastoviza" ~ "Chelidonichthys lastoviza",
+    taxon_name == "Pseudopentaceros richardsoni" ~ "Pentaceros richardsoni",
+    # go to genus or family for those with missing data; AFCD will average 
+    grepl("Callorhinchus", taxon_name) ~ "callorhinchus",
+    # go to family for Hydrolagus; only one species in genus and a few chimaeras
+    grepl ("Hydrolagus", taxon_name) ~ "chimaeridae",
+    taxon_name == "Echinus esculentus" ~ "echinidae",
+    taxon_name == "Myxine glutinosa" ~ "myxinidae",
+    
     TRUE ~ taxon_name
   )
   )
@@ -37,6 +43,10 @@ dbem_spp_afcd_nutr <-  species_nutrients (spp_list$afcd_name,
   ) %>%
   # reconvert species names to match data
   mutate (afcd_name = firstup(species))
+
+
+##########################
+# reconcile missing spp
 
 # species with missing data--18
 missing_nutr <- dbem_spp_afcd_nutr %>%
@@ -51,4 +61,16 @@ missing_nutr_spp[which(!missing_nutr_spp %in% afcd_sci$sciname)]
 # Liza is now Chelon; keep aurata, haematocheila is haematocheilus; C. Klunziringeri is "Planiliza klunzingeri"
 # "Theragra chalcogramma" is now "Gadus chalcogrammus"
 # "Trigloporus lastoviza" is "Chelidonichthys lastoviza"
-# hydrolagus genus is missing, and don't have hagfish
+
+# still missing: 
+# "Callorhinchus capensis"--has C. callorhynchus
+# "Echinus esculentus"--in family echinidae, has Paracentrotus lividus; Loxechinus albus; Sterechinus neumayeri
+# hydrolagus genus --has H. affinis, Chimaera monstrosa, and C. phantasma in chimaeridae 
+#hagfish; does have an inshore NW Pacific spp, Eptatretus burgeri
+
+
+family_test <- species_nutrients ("callorhinchus",
+                                                         prep = c ("raw", NA),
+                                                         part = c("muscle tissue", "whole"),
+                                                         nut = c("DHA_EPA", "Calcium", "Iron, total", "Selenium", "Zinc", "Vitamin_a_combined", "Protein_total_combined")
+) 
